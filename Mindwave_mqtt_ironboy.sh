6d0138531464cdabbd69803d8e11b0bb4538dfa4
Kipom Kim <kim@piconewton.com>
2018-11-26 03:23:04 +0900

    modified

--- Mindwave_mqtt_ironboy.sh
+++ Mindwave_mqtt_ironboy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd /home/kbri/BMI_Factory-dev-Nov
-python3 mindwave_mqtt_Ironboy.py 98:07:2D:7F:DD:CB
+python3 mindwave_mqtt_Ironboy.py 98:07:2D:7F:4C:2B # green
+#python3 mindwave_mqtt_Ironboy.py 98:07:2D:80:7B:3E # yellow
