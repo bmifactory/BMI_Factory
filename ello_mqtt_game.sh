40f152dee5d79395482fa6047044a1020326fdc4
bmifactory <kipom.kim@gmail.com>
2019-06-24 11:40:36 +0900

    modified

--- ello_mqtt_game.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /home/kbri/BMI_Factory-Dev-2019-06
-python3 tello_mqtt_game.py
--- indwave_mqtt_tello.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd /home/kbri/BMI_Factory-Dev-2019-06
-#python3 mindwave_mqtt_tello.py 98:07:2D:80:7B:3E # yellow
-#python3 mindwave_mqtt_tello.py 98:07:2D:7F:DD:86 # red
-python3 mindwave_mqtt_tello.py 98:07:2D:7F:4C:2B # green
