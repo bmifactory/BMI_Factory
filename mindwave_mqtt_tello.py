f819986337fb594b8c9346f6f60aa3e2e4064dcf
bmifactory <kipom.kim@gmail.com>
2019-06-24 11:20:14 +0900

    modified

--- mindwave_mqtt_tello.py
+++ mindwave_mqtt_tello.py
@@ -1,5 +1,6 @@
 '''
 Created on 2018. 10. 24.
+Modified on 2019. 06. 24
 @author: Kipom Kim
 '''
 # -*- coding: utf-8 -*-
@@ -38,7 +39,7 @@ raw_eeg = True
 gain = 1.0
 attention_value = 0
 Th_attention = 50
-Th2_attention = 90
+#Th2_attention = 90
 attention_duration = 0
 duration_off = 20
 duration_level1 = 16
@@ -51,9 +52,9 @@ os.environ['SDL_VIDEO_WINDOW_POS']="%d, %d" % (0,0)
 bg_file = "Drone_1360x768_start.jpg"
 bg_file_close = "Drone_1360x768_start.jpg"
 bg_file_open = "Drone_1360x768_open.jpg"
-message_range = 20
-message_list = list(range(message_range))
-m_event = 0
+message_max = 20
+message_list = list(range(message_max))
+event_log = 0
 takeoff_flag = False
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -90,7 +91,8 @@ def setup():
     whiteColor = pygame.Color(255, 255, 255)
     greenColor = pygame.Color(0, 255, 0)
     eegColor = pygame.Color(255, 255, 0)
-    attentionColor = pygame.Color(10, 85, 145)
+    #attentionColor = pygame.Color(10, 85, 145)
+    attentionColor = pygame.Color(0, 255, 0)
 
     fpsClock = pygame.time.Clock()
     pygame.display.set_caption("Tello Neurosky")
@@ -99,7 +101,7 @@ def setup():
     else:
         window = pygame.display.set_mode((1360, 768), pygame.RESIZABLE)
     background_img = pygame.image.load(bg_file)
-    pygame_update(m_event)
+    pygame_update(event_log)
 
 def pygame_update(message_lane):
     global message_list, window, message_img, background_img
@@ -107,16 +109,16 @@ def pygame_update(message_lane):
     global takeoff_flag
     font = pygame.font.Font("bgothl.ttf", 20)
     window.blit(background_img, (0, 0))
-    for i in range(20):
+    for i in list(range(message_max)):
         if i < message_lane - 1:
             message_img = font.render("# "+str(message_list[i]), False, whiteColor)
-            window.blit(message_img, (1200, 100 + i * 30))
+            window.blit(message_img, (1250, 125 + i * 30))
         elif i == message_lane - 1:
             message_img = font.render("# "+str(message_list[i]), False, greenColor)
-            window.blit(message_img, (1200, 100 + i * 30))
+            window.blit(message_img, (1250, 125 + i * 30))
         else:
             message_img = font.render("", False, whiteColor)
-            window.blit(message_img, (1200, 80 + i * 30))
+            window.blit(message_img, (1250, 125 + i * 30))
 
     if int(attention_value) > 100:
         attention_value = 100
@@ -133,19 +135,19 @@ def pygame_update(message_lane):
         attention_txt_img = font.render("Attention", False, whiteColor)
         duration_value_img = font.render(str(attention_duration), False, whiteColor)
         duration_txt_img = font.render("Charging", False, whiteColor)
-        window.blit(attention_txt_img, (280, 700))
+        window.blit(attention_txt_img, (280, 680))
         window.blit(attention_value_img, (320, 650))
-        window.blit(duration_txt_img, (930, 700))
+        window.blit(duration_txt_img, (930, 680))
         window.blit(duration_value_img, (970, 650))
         angle_attention = (2*attention_value-130+random.randint(0, 5))*0.01745329252
         draw_gauge_needle(358, 582, angle_attention, 160, 6)
         angle_duration = (2*attention_duration-42)*0.01745329252
         draw_gauge_needle(1005, 582, angle_duration, 160, 6)
     else:
-        attention_value_img = font.render(str(attention_value), False, blackColor)
-        attention_txt_img = font.render("Attention", False, blackColor)
-        duration_value_img = font.render(str(attention_duration), False, blackColor)
-        duration_txt_img = font.render("Charging", False, blackColor)
+        attention_value_img = font.render(str(attention_value), False, blueColor)
+        attention_txt_img = font.render("Attention", False, blueColor)
+        duration_value_img = font.render(str(attention_duration), False, blueColor)
+        duration_txt_img = font.render("Charging", False, blueColor)
         window.blit(attention_txt_img, (280, 600))
         window.blit(attention_value_img, (320, 550))
         window.blit(duration_txt_img, (930, 600))
@@ -156,12 +158,11 @@ def pygame_update(message_lane):
             attention_duration = 21.1
         length_duration = 10*(attention_duration+1)
         draw_gauge_bar(1005, 532, 1, length_duration, 100)
-
     if control_mode is True:
         pass
     else:
         mode_img = font.render("press F1 for BMI control mode", False, whiteColor)
-        window.blit(mode_img, (800, 700))
+        window.blit(mode_img, (800, 720))
     pygame.display.update()
 
 def draw_gauge_needle(center_x, center_y, ang, length, width):
@@ -178,17 +179,16 @@ def draw_gauge_bar(center_x, center_y, dir, length, width):
         end_y = center_y - length
     pygame.draw.line(window, redColor, (center_x, center_y), (end_x, end_y), width)
 
-def m_event_update(message):
-    global m_event, message_list
-    message_list[m_event] = message
-    pygame_update(m_event)
-    if m_event >= message_range - 1:
-        m_event = 0
-    else:
-        m_event = m_event + 1
+def event_log_update(message):
+    global event_log, message_list
+    message_list[event_log] = message
+    pygame_update(event_log)
+    event_log = event_log + 1
+    if event_log == message_max:
+        event_log = 0
 
 def main():
-    global battary, m_event, tello, iteration, background_img, mqttClient
+    global battary, event_log, tello, iteration, background_img, mqttClient
     global attention_duration, attention_value, control_mode, takeoff_flag
     # spectra = []
     quit = False
@@ -203,7 +203,7 @@ def main():
         if len(recorder.attention) > 0:
             attention_value = int(recorder.attention[-1])
             attention_value = gain * attention_value
-            pygame_update(m_event)
+            pygame_update(event_log)
             # Show raw EEG signal
             if raw_eeg:
                 lv = 0
@@ -226,7 +226,7 @@ def main():
                     if attention_duration >= duration_off:
                         mqtt_command = 'takeoff'
                         # print('%s' % mqtt_command)
-                        m_event_update(mqtt_command)
+                        event_log_update(mqtt_command)
                         if mqtt_connected:
                             mqttClient.publish(Topic_name, mqtt_command)
                         takeoff_flag = True
@@ -237,13 +237,13 @@ def main():
                 else:
                     if attention_duration == duration_level1:
                         # print('level1')
-                        m_event_update('level1')
+                        event_log_update('level1')
                         if mqtt_connected:
                             command = 'level1'
                             mqttClient.publish(Topic_name, command)
                     elif attention_duration == duration_level2:
                         # print('level2')
-                        m_event_update('level2')
+                        event_log_update('level2')
                         if mqtt_connected:
                             mqtt_command = 'level2'
                             mqttClient.publish(Topic_name, mqtt_command)
@@ -266,21 +266,19 @@ def main():
                 elif event.key == K_F1:
                     if control_mode == True:
                         control_mode = False
-                        #background_img = pygame.image.load(bg_file_close)
                     else:
                         control_mode = True
-                    background_img = pygame.image.load(bg_file_close)
                 elif event.key == K_t:
                     mqtt_command = 'takeoff'
                     takeoff_flag = True
-                    m_event_update(mqtt_command)
+                    event_log_update(mqtt_command)
                     if control_mode:
                         if mqtt_connected:
                             mqttClient.publish(Topic_name, mqtt_command)
-                        background_img = pygame.image.load(bg_file_open)
+                    background_img = pygame.image.load(bg_file_open)
                 elif event.key == K_l:
                     mqtt_command = 'land'
-                    m_event_update(mqtt_command)
+                    event_log_update(mqtt_command)
                     if mqtt_connected:
                         mqttClient.publish(Topic_name, mqtt_command)
                     background_img = pygame.image.load(bg_file)
@@ -288,47 +286,47 @@ def main():
                     control_mode = False
                 elif event.key == K_LEFT:
                     mqtt_command = 'left'
-                    m_event_update(mqtt_command)
+                    event_log_update(mqtt_command)
                     if mqtt_connected:
                         mqttClient.publish(Topic_name, mqtt_command)
                 elif event.key == K_RIGHT:
                     mqtt_command = 'right'
-                    m_event_update(mqtt_command)
+                    event_log_update(mqtt_command)
                     if mqtt_connected:
                         mqttClient.publish(Topic_name, mqtt_command)
                 elif event.key == K_UP:
                     mqtt_command = 'forward'
-                    m_event_update(mqtt_command)
+                    event_log_update(mqtt_command)
                     if mqtt_connected:
                         mqttClient.publish(Topic_name, mqtt_command)
                 elif event.key == K_DOWN:
                     mqtt_command = 'back'
-                    m_event_update(mqtt_command)
+                    event_log_update(mqtt_command)
                     if mqtt_connected:
                         mqttClient.publish(Topic_name, mqtt_command)
                 elif event.key == K_HOME:
                     mqtt_command = 'cw'
-                    m_event_update(mqtt_command)
+                    event_log_update(mqtt_command)
                     if mqtt_connected:
                         mqttClient.publish(Topic_name, mqtt_command)
                 elif event.key == K_END:
                     mqtt_command = 'ccw'
-                    m_event_update(mqtt_command)
+                    event_log_update(mqtt_command)
                     if mqtt_connected:
                         mqttClient.publish(Topic_name, mqtt_command)
                 elif event.key == K_PAGEUP:
                     mqtt_command = 'up'
-                    m_event_update(mqtt_command)
+                    event_log_update(mqtt_command)
                     if mqtt_connected:
                         mqttClient.publish(Topic_name, mqtt_command)
                 elif event.key == K_PAGEDOWN:
                     mqtt_command = 'down'
-                    m_event_update(mqtt_command)
+                    event_log_update(mqtt_command)
                     if mqtt_connected:
                         mqttClient.publish(Topic_name, mqtt_command)
                 elif event.key == K_f:
                     mqtt_command = 'flip'
-                    m_event_update(mqtt_command)
+                    event_log_update(mqtt_command)
                     if mqtt_connected:
                         mqttClient.publish(Topic_name, mqtt_command)
         # Display update
