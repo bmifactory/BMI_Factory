0888f5a43517983031d4ca3489657e8f6a0bcfa2
Kipom Kim <kipom.kim@gmail.com>
2019-06-25 16:41:41 +0900

    Update Mindwave_mqtt_tello.sh

--- Mindwave_mqtt_tello.sh
+++ Mindwave_mqtt_tello.sh
@@ -2,4 +2,5 @@
 cd /home/kbri/BMI_Factory-Dev-2019-06
 #python3 mindwave_mqtt_tello.py 98:07:2D:80:7B:3E # yellow
 #python3 mindwave_mqtt_tello.py 98:07:2D:7F:DD:86 # red
-python3 mindwave_mqtt_tello.py 98:07:2D:7F:4C:2B # green
+#python3 mindwave_mqtt_tello.py 98:07:2D:7F:4C:2B # green
+python3 mindwave_mqtt_tello.py 98:07:2D:80:4A:A6 # seoul
